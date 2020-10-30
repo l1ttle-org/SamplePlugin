8018ffdcf64aded862e304fa03ff139243e3b7ff
l1ttleO <61277953+l1ttleO@users.noreply.github.com>
2020-10-30 11:15:04 +0500

    Remove syntax check

--- src/main/java/org/l1ttle/testplugin/commands/testperm.java
+++ src/main/java/org/l1ttle/testplugin/commands/testperm.java
@@ -12,11 +12,8 @@ public class testperm extends Command {
     }
 
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
-
-        if (args.length != 1) {
-            throw new IllegalArgumentException("Syntax: /testperm <permission>"); }
-
-        else if (sender.hasPermission(Arrays.toString(args))) {
+        
+        if (sender.hasPermission(Arrays.toString(args))) {
             sender.sendMessage("You have permission " + Arrays.toString(args));
             return true; }
 
